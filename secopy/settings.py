a6d3dcab11f47413b847c0804ca98f1129aae5f0
Jan Zadrapa <xzadra03@stud.fit.vutbr.cz>
2022-03-09 17:40:48 +0100

    Updated settings

--- secopy/settings.py
+++ secopy/settings.py
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yqm=ri-c6ju(&i8_sc68b^07fnm(o#n)@*ls255tis5fss@udp'
+#SECRET_KEY = 'django-insecure-yqm=ri-c6ju(&i8_sc68b^07fnm(o#n)@*ls255tis5fss@udp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'heroku-secopy.herokuapp.com'
+]
 
 
 # Application definition
